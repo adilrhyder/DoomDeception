af907e01f9050ee2a1c2145a567ab729492ca013
R-Nade <114340940+RaahimNadeem@users.noreply.github.com>
2023-12-01 21:42:13 +0500

    Pause and Main Menu

---
+++ Assets/SettingsMenu.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    // Start is called before the first frame update
+    public void SetVolume (float volume)
+    {
+        Debug.Log(volume);
+        audioMixer.SetFloat("volume", volume);
+
+    }
+}
